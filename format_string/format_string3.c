6c25733b96a534c922b958c55d7b054b651c3bf1
choisungwook <kgg1959@naver.com>
2018-02-09 18:47:13 +0900

    level11

--- format_string/format_string3.c
+++ format_string/format_string3.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
     int i = 7;
     char buf[128];
     strcpy(buf, argv[1]);
-    printf(argv);
-    printf("i : %p\n i : %d", &i, i);
+    printf(buf);
+    printf("\ni : %p\ni : %d\n", &i, i);
     return 0;
 }
